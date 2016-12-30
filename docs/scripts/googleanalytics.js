e3c599cee2d3134e4938cbb5ba4559b21778a979
5jt <sjt@5jt.com>
2016-12-30 08:25:36 +0000

    GA code; copyright notice

--- docs/scripts/googleanalytics.js
+++ docs/scripts/googleanalytics.js
@@ -1,11 +1,8 @@
 /* GOOGLE ANALYTICS CODE FOR CODE.KX.COM */
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-3462586-1', 'auto', {'allowLinker': true});
-  ga('require', 'linker');
-  ga('linker:autoLink', ['kx.com', 'kxcommunity.com'] );
-  ga('require', 'displayfeatures');
-  ga('send', 'pageview');
+ga('create', 'UA-3462586-4', 'auto');
+ga('send', 'pageview');
