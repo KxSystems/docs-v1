fc434550267df438293670eb303fd45b385db739
5jt <sjt@5jt.com>
2017-07-22 17:01:38 +0100

    GCSE search box style

--- docs/scripts/theme.js
+++ docs/scripts/theme.js
@@ -11,6 +11,14 @@ $(function() {
 		}
 	});
 
+	// // Supplement local search: send query to Google Search on Return
+	// $("[data-md-component=query]").change(function() {
+	// 	var qry = "http://www.google.com/search?q=";
+	// 	qry += "site%3Acode.kx.com/q/+"; // non-portable and excludes wiki
+	// 	window.location = qry + $(this).val();
+	// 	return false; // prevents submitting form to server
+	// });
+
 	// cse.google.com Google Custom Search Engine
 	// replace MkDocs Search form with GCSE container
 	$('form[name="search"]').parent().parent().replaceWith('<gcse:search id="gcse_search"></gcse:search>');
@@ -20,23 +28,13 @@ $(function() {
 	gcse.type = 'text/javascript';
 	gcse.async = true;
 	gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-	// after the GCSE adsense script runs, adapt GCSE style to MkDocs
-	// $(document).on("afterscriptexecute", function() {
-	$(document).on('afterscriptexecute', function(e) {
-		if(e.target.tagName.toLowerCase() == 'script' && e.target.src.slice(0,29) === 'http://cse.google.com/adsense') {
-		// if(e.target.tagName.toLowerCase() == 'script') {
-			console.log('Matched: ' + e.target.src.slice(0,29));
-			console.log('Loaded: ' + e.target.src);
-			$('#gcse_search').css('width','300px');
-			console.log('Set search-box width');
-			var style = $('<style>.cse .gsc-control-cse, .gsc-control-cse {background-color: transparent; border-color: transparent;}.gsc-search-button {display: none;}</style>');
-			$('html > head').append(style);
-		}
-	});
-
 	// insert the master GCSE script
 	var s = document.getElementsByTagName('script')[0];
-	console.log('Insert new script');
+	// console.log('Insert new script');
 	s.parentNode.insertBefore(gcse, s);
-
+	// override GCSE style rules to 
+	var style = '.cse .gsc-control-cse, .gsc-control-cse {background-color: transparent !important; border-color: transparent !important;}';
+	style += '.gsc-search-button {display: none;}';
+	style += ' #gcse_search {width: 300px !important}';
+	$('html > head').append($('<style>'+style+'</style>'));
 });
